796f8f4703e2fed2e8e37c6a731a4f73474bdf7e
Anthony Colangelo <me@acolangelo.com>
2012-10-15 23:26:47 -0400

    Added jRespond to script.js.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 $(function() {
 	var jPanelMenu = $.jPanelMenu();
-	jPanelMenu.on();
+	var jR = jRespond([
+		{
+			label: 'small',
+			enter: 0,
+			exit: 800
+		},{
+			label: 'medium',
+			enter: 800,
+			exit: 1000
+		},{
+			label: 'large',
+			enter: 1000,
+			exit: 10000
+		}
+	]);
+	jR.addFunc({
+		breakpoint: 'small',
+		enter: function() {
+			jPanelMenu.on();
+		},
+		exit: function() {
+			jPanelMenu.off();
+		}
+	});
 });
