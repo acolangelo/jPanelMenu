98e798ae7bf7857d001351fe24665e20c699a336
Anthony Colangelo <me@acolangelo.com>
2012-11-02 00:36:41 -0400

    Began documentation site.

---
+++ docs/js/script.js
@@ -0,0 +1,33 @@
+var jPanelMenu = {};
+$(function() {
+	// jPanelMenu = $.jPanelMenu({
+	// 	menu: 'header.main nav'
+	// });
+	// var jR = jRespond([
+	// 	{
+	// 		label: 'small',
+	// 		enter: 0,
+	// 		exit: 800
+	// 	},{
+	// 		label: 'medium',
+	// 		enter: 800,
+	// 		exit: 1000
+	// 	},{
+	// 		label: 'large',
+	// 		enter: 1000,
+	// 		exit: 10000
+	// 	}
+	// ]);
+	// jR.addFunc({
+	// 	breakpoint: 'small',
+	// 	enter: function() {
+	// 		jPanelMenu.on();
+	// 	},
+	// 	exit: function() {
+	// 		jPanelMenu.off();
+	// 	}
+	// });
+	// $(document).on('click',jPanelMenu.menu + ' li a',function(e){
+	// 	if ( jPanelMenu.isOpen() ) { jPanelMenu.close(true); }
+	// });
+});
