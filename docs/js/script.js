6189ecd17cec93df16f311fcda87f078b0eaf752
Anthony Colangelo <me@acolangelo.com>
2012-11-04 13:52:59 -0500

    Added off/on toggle for jPanelMenu.

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,6 +8,19 @@ $(function() {
 	$(document).on('click',jPanelMenu.menu + ' li a',function(e){
 		if ( jPanelMenu.isOpen() && $(e.target).attr('href').substring(0,1) == '#' ) { jPanelMenu.close(); }
 	});
+	$(document).on('click','#trigger-off',function(e){
+		jPanelMenu.off();
+		$('html').css('padding-top','40px');
+		$('#trigger-on').remove();
+		$('body').append('<a href="" title="Re-Enable jPanelMenu" id="trigger-on">Re-Enable jPanelMenu</a>');
+		e.preventDefault();
+	});
+	$(document).on('click','#trigger-on',function(e){
+		jPanelMenu.on();
+		$('html').css('padding-top',0);
+		$('#trigger-on').remove();
+		e.preventDefault();
+	});
 	// var jR = jRespond([
 	// 	{
 	// 		label: 'small',
